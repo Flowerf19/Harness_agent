a74b9eb0885f677a5d0b69d2b843fe7daedddfbf
flowerf19 <hoaf.n.v@gmail.com>
2026-02-26 21:52:50 +0700

    refactor: Update services __init__ to remove old ConversationManager references

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,17 +1,19 @@
 # filepath: discord-bot-gemini/src/services/__init__.py
+from .activity_monitor import ActivityMonitor
 from .anti_spam_service import AntiSpamService
-from .conversation_manager import ConversationManager
 from .gemini_service import GeminiService
 from .lm_studio_service import LMStudioService
+from .memory_background_service import MemoryBackgroundService
+from .memory_manager import MemoryManager
 from .message_processor import MessageProcessor
 from .ollama_service import OllamaService
 from .qwen_service import QwenService
 from .relationship_service import RelationshipService
 from .summary_service import SummaryService
+from .working_memory_service import WorkingMemoryService
 
 __all__ = [
     "AntiSpamService",
-    "ConversationManager",
     "GeminiService",
     "LMStudioService",
     "MessageProcessor",
@@ -19,4 +21,8 @@ __all__ = [
     "QwenService",
     "RelationshipService",
     "SummaryService",
+    "MemoryManager",
+    "MemoryBackgroundService",
+    "WorkingMemoryService",
+    "ActivityMonitor",
 ]
